0f65618387f55438b8e712ce364dcfc5edd1bfa4
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-05-11 13:13:50 -0500

    Initial Commit

---
+++ SimpleNotes/SimpleNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNotesApp.swift
+//  SimpleNotes
+//
+//  Created by Justin Hold on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
